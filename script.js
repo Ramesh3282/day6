4cbaf2aabe16231ddc56fc87c243c0b24768699b
Ramesh3282 <ramesharthanari.a@gmail.com>
2023-02-13 21:26:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+alert("I’m invoked!");
+
+alert("I'm JavaScript!");
+alert('Hello') // this line is not having semicolon
+alert(`Wor
+ ld`)
+alert(3 +
+1
++ 2); // this is multiple line code and its working
+
+
+// let admin=9, fname=10.5; 
+// fname = "Guvi";
+// lname = "geek"
+// admin = fname+lname;
+// alert( admin ); // "Guvi geek"
+
+let fname=10.5; 
+fname = "Guvi";
+lname = "geek"
+let name = fname+lname;
+alert( `hello ${name}` );
+
+let a = +prompt("First number?");
+let b = +prompt("Second number?");
+alert(a + b);
